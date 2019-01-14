3855667cd73a02d7a83f8e27e12b0af5b7e23523
SkodRander <frederik396@gmail.com>
2019-01-14 14:13:40 +0100

    new stuff for potentiometer and btn

--- projektv1/projektv1.ino
+++ projektv1/projektv1.ino
@@ -2,34 +2,42 @@
 #include <DHTesp.h>
 #include <ESP8266WiFi.h> 
 #include <Wire.h> 
-#include <ThingSpeak.h>
+#define btn D4
+//#include <ThingSpeak.h>
 
 float temperature;
 float humidity;
+int n = 0; 
+float tmpin;
+boolean buttonstate = false;
 
 const char* ssid = "OnePlus6";
 const char* password = "skodrander";
-unsigned long channelID = 673873; //your channal
+
+/*unsigned long channelID = 673873; //your channal
 const char* myWriteAPIKey = "5SUTO406JKZVF2EN"; 
 const char* server = "api.thingspeak.com";
 const int postingInterval = 20 * 1000; // post data every 20 seconds
 
 struct profile {
-  int id;
-  int temp;
+  const int id;
+  const int temp;
   boolean checked_in;
-}
+}*/
 
 WiFiClient client;
 DHTesp dht;
 LiquidCrystal_I2C lcd(0x27,16,2); 
+
 void setup() {
   dht.setup(0, DHTesp::DHT22);// Connect DHT sensor to GPIO 0 (D3)
   lcd.begin();
   lcd.backlight();
+  pinMode(D4,INPUT_PULLUP);
   Serial.begin(115200);
   WiFi.begin(ssid, password);
   lcd.print("Connecting");
+  Serial.println("hej");
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(1000);
@@ -43,8 +51,31 @@ void setup() {
 }
 
 void loop() {
-  data_to_lcd();
-  delay(1000);
+  //data_to_lcd();  
+
+  lcd.clear(); 
+  lcd.setCursor(0,0);
+  lcd.print("Setup Profile: ");
+  delay(5000);
+  while(buttonstate == false) {
+    lcd.clear();
+    lcd.print("What temp do you");
+    lcd.setCursor(0,1);
+    lcd.print("prefer?");
+    lcd.setCursor(10,1);
+    tmpin = analogRead(A0);
+    tmpin = tmpin/65.0+12.0;
+    lcd.print(tmpin,1);
+    
+    if(digitalRead(btn) == LOW){
+      buttonstate = true; 
+    }
+    delay(200);
+  }
+  
+  Serial.println(tmpin);
+  
+  delay(5000);
 }
 
 void data_to_lcd(){
@@ -68,6 +99,27 @@ void read_RFID(){
   
 }
 
-void profile_setup(){
+void profile_setup(int id_n){
+  n++;
+  lcd.clear(); 
+  lcd.setCursor(0,0);
+  lcd.print("Setup Profile: ");
+  delay(5000);
+  while(buttonstate == false) {
+    lcd.clear();
+    lcd.print("What temp do you");
+    lcd.setCursor(0,1);
+    lcd.print("prefer?");
+    lcd.setCursor(10,1);
+    tmpin = analogRead(A0);
+    tmpin = tmpin/65.0+12.0;
+    lcd.print(tmpin,1);
+    
+    if(digitalRead(btn) == LOW){
+      buttonstate = true;
+    }
+    delay(200);
+  }
+  
   
 }
